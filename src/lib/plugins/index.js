f6c70b6b3fbdccac2ce03b192cb9d06b114edd21
Manaia Junior <manaiajr.23@gmail.com>
2021-05-31 23:43:58 -0300

    Add local IP

--- src/lib/plugins/index.js
+++ src/lib/plugins/index.js
@@ -1,12 +1,9 @@
 import hostname from './hostname'
 import ip from './ip'
+import localip from './localip'
 import memory from './memory'
-// Import Uptime from './uptime'
+import battery from './battery'
 import cpu from './cpu'
 import network from './network'
-import battery from './battery'
-// Import Time from './time'
-// Import Docker from './docker'
-import spotify from './spotify'
 
-export default [hostname, ip, memory, battery, cpu, network, spotify]
+export default [hostname, ip, localip, memory, battery, cpu, network]
